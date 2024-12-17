d496365cbf8fdf0717e7ee295f668d0ea6b7a1b9
Mamadou Dioulde Barry <brry00802@gmail.com>
2024-12-17 09:48:31 +0000

    adding some templates

--- myportfolio/admin.py
+++ myportfolio/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
+from .models import Skill, Education, Experience, Project, Contact
 
-# Register your models here.
+@admin.register(Skill)
+class SkillAdmin(admin.ModelAdmin):
+    list_display = ('name', 'proficiency')
+    search_fields = ('name',)
+
+@admin.register(Education)
+class EducationAdmin(admin.ModelAdmin):
+    list_display = ('degree', 'field_of_study', 'institution', 'start_date', 'end_date')
+    search_fields = ('institution', 'degree', 'field_of_study')
+
+@admin.register(Experience)
+class ExperienceAdmin(admin.ModelAdmin):
+    list_display = ('title', 'company', 'start_date', 'end_date')
+    search_fields = ('title', 'company')
+
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ('title', 'start_date', 'end_date')
+    search_fields = ('title',)
+    filter_horizontal = ('technologies',)
+
+@admin.register(Contact)
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ('email', 'phone_number')
+    search_fields = ('email',)
